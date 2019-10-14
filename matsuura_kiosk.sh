7a7cd431649bc36c22ac3d4711555b562ff46543
sudobob <bob.coggeshall@gmail.com>
2019-10-14 08:01:14 -0400

    Starting chromium from systemd didn't work.
    Using lxsessions/autostart instead

--- matsuura_kiosk.sh
+++ matsuura_kiosk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #
-# https://pimylifeup.com/raspberry-pi-kiosk/
 #
 xset s noblank
 xset s off
@@ -13,9 +12,7 @@ source /home/pi/matsuura_uploader/.env
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost/login\?username=$USER_NAME\&password=$PASSWORD
+DISPLAY=:0.0
 
-#while true; do
-#  xdotool keydown ctrl+r; xdotool keyup ctrl+r;
-#  sleep 10
-#done
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost/login\?username=$USER_NAME\&password=$PASSWORD 
+
