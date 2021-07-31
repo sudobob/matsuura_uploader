2e4246544ae5675c30862b6ac2c5ef7c9967e2e5
curtwelch <curt@kcwc.com>
2021-07-31 13:03:58 -0400

    Improve first-line status to skip blanks and %. (not tested)

--- app.py
+++ app.py
@@ -126,8 +126,18 @@ def upload_file():
     return render_template("index.html")
 
 def get_first_line(fn):
-  f = open(os.path.join(upload_path,fn),'r')
-  return f.readline().rstrip()
+    f = open(os.path.join(upload_path,fn),'r')
+    # Skip blank lines and skip '%' line
+    while True:
+        line = f.readline()
+        if line == '':    # EOF
+            break
+        line = line.rstrip()
+        if line == '%' or line == '':
+            continue
+        break
+
+    return line
 
 def get_files_uploaded():
     file_names = os.listdir(upload_path)
